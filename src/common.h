9ba01926ced2bbd42b06e3668579bdf0dfbd0c6a
mpadge <mark.padgham@email.com>
2018-03-13 21:22:27 +0100

    forgot src/common.h :ambulance:

---
+++ src/common.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <limits>
+
+const float INFINITE_FLOAT =  std::numeric_limits<float>::max ();
+const double INFINITE_DOUBLE =  std::numeric_limits<double>::max ();
+const int INFINITE_INT =  std::numeric_limits<int>::max ();
